a5e3173d76a6d318c5010a290876b20fbe4d568c
Arafat <arafat6t9@gmail.com>
2026-05-21 19:32:38 +0600

    Fixed the pictures

--- src/data/photos.js
+++ src/data/photos.js
@@ -2,26 +2,38 @@ const photos = [
 
   {
     image:
-      "https://live.staticflickr.com/8612/buddyicons/100247052@N07.jpg",
+      "https://live.staticflickr.com/7823/47027544081_8b342ef16b_c.jpg",
     category: "Street",
   },
 
   {
     image:
-      "https://live.staticflickr.com/8612/buddyicons/100247052@N07.jpg",
+      "https://live.staticflickr.com/8609/29355277806_60ed06967b_c.jpg",
     category: "Wedding",
   },
 
   {
     image:
-      "https://live.staticflickr.com/8612/buddyicons/100247052@N07.jpg",
+      "https://live.staticflickr.com/398/20369656291_09ac787b01_b.jpg",
     category: "Fashion",
   },
 
   {
     image:
-      "https://live.staticflickr.com/8612/buddyicons/100247052@N07.jpg",
+      "https://live.staticflickr.com/8321/28434260303_555403fe4e_b.jpg",
     category: "Travel",
+  },
+
+  {
+    image:
+      "https://live.staticflickr.com/669/31595713915_2f4fa42ac1_b.jpg",
+    category: "Miniature",
+  },
+
+  {
+    image:
+      "https://live.staticflickr.com/341/32106876966_8e25ccfde2_b.jpg",
+    category: "Landscape",
   },
 
 ]
